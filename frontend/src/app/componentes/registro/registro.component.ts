91a40351538a7e29eca93749917397dec37b98cb
Diego <juan.zegarra9@unmsm.edu.pe>
2020-08-10 20:17:27 -0500

    Front de Index y Registro

--- frontend/src/app/componentes/registro/registro.component.ts
+++ frontend/src/app/componentes/registro/registro.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.sass']
 })
 export class RegistroComponent implements OnInit {
+  nombre: String;
+  correo: String;
+  email: String;
+  password: String;
 
   constructor() { }
 
